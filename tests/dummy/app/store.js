f1f997c90134abe90e8ab4fed07b11f1938dfda3
Arnis Vuskans <ampatspell@gmail.com>
2018-04-07 21:09:15 +0300

    eslint

--- tests/dummy/app/store.js
+++ tests/dummy/app/store.js
@@ -1,13 +1,13 @@
 import Store from 'ember-cli-zuglet/store';
 
-let travis = {
-  apiKey: "AIzaSyAvm8v8kXS3iHsg3EBxAk-mZZcYdeOuj3E",
-  authDomain: "ember-cli-zug-travis.firebaseapp.com",
-  databaseURL: "https://ember-cli-zug-travis.firebaseio.com",
-  projectId: "ember-cli-zug-travis",
-  storageBucket: "ember-cli-zug-travis.appspot.com",
-  messagingSenderId: "715071933357"
-};
+// let travis = {
+//   apiKey: "AIzaSyAvm8v8kXS3iHsg3EBxAk-mZZcYdeOuj3E",
+//   authDomain: "ember-cli-zug-travis.firebaseapp.com",
+//   databaseURL: "https://ember-cli-zug-travis.firebaseio.com",
+//   projectId: "ember-cli-zug-travis",
+//   storageBucket: "ember-cli-zug-travis.appspot.com",
+//   messagingSenderId: "715071933357"
+// };
 
 let dev = {
   apiKey: "AIzaSyApr48AJWch97DybXXVhF53LJttudP8E2Y",
