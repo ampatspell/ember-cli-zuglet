e844c022a4a5cf8caeb37c733dbe3cacde757ad3
Arnis Vuskans <ampatspell@gmail.com>
2018-04-07 21:08:32 +0300

    move back to zug.appspot

--- tests/dummy/app/store.js
+++ tests/dummy/app/store.js
@@ -1,20 +1,25 @@
 import Store from 'ember-cli-zuglet/store';
 
+let travis = {
+  apiKey: "AIzaSyAvm8v8kXS3iHsg3EBxAk-mZZcYdeOuj3E",
+  authDomain: "ember-cli-zug-travis.firebaseapp.com",
+  databaseURL: "https://ember-cli-zug-travis.firebaseio.com",
+  projectId: "ember-cli-zug-travis",
+  storageBucket: "ember-cli-zug-travis.appspot.com",
+  messagingSenderId: "715071933357"
+};
+
+let dev = {
+  apiKey: "AIzaSyApr48AJWch97DybXXVhF53LJttudP8E2Y",
+  authDomain: "ember-cli-zug.firebaseapp.com",
+  databaseURL: "https://ember-cli-zug.firebaseio.com",
+  projectId: "ember-cli-zug",
+  storageBucket: "ember-cli-zug.appspot.com",
+  messagingSenderId: "102388675337"
+};
+
 const options = {
-  firebase: {
-    // apiKey: "AIzaSyApr48AJWch97DybXXVhF53LJttudP8E2Y",
-    // authDomain: "ember-cli-zug.firebaseapp.com",
-    // databaseURL: "https://ember-cli-zug.firebaseio.com",
-    // projectId: "ember-cli-zug",
-    // storageBucket: "ember-cli-zug.appspot.com",
-    // messagingSenderId: "102388675337"
-    apiKey: "AIzaSyAvm8v8kXS3iHsg3EBxAk-mZZcYdeOuj3E",
-    authDomain: "ember-cli-zug-travis.firebaseapp.com",
-    databaseURL: "https://ember-cli-zug-travis.firebaseio.com",
-    projectId: "ember-cli-zug-travis",
-    storageBucket: "ember-cli-zug-travis.appspot.com",
-    messagingSenderId: "715071933357"
-  },
+  firebase: dev,
   firestore: {
     persistenceEnabled: true
   }
