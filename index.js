2f205f5b0959cab37b876143e6c7d93a83c237e8
Arnis Vuskans <ampatspell@gmail.com>
2018-04-07 22:25:47 +0300

    add luxon as a dep. app.import luxon as amd

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 let create = require('broccoli-file-creator');
 let mergeTrees = require('broccoli-merge-trees');
 var Funnel = require('broccoli-funnel');
-let firebase = require('firebase');
 let pkg = require('./package.json');
 let path = require('path');
+
+let isFirebaseEnabled = true;
+let isLuxonEnabled = true;
 
 module.exports = {
   name: 'ember-cli-zuglet',
@@ -14,11 +16,23 @@ module.exports = {
   },
   included(app) {
     this._super.apply(this, arguments);
-    app.import('vendor/ember-cli-zuglet/firebase.js');
-    app.import('vendor/ember-cli-zuglet/firebase.js.map');
-    app.import('vendor/ember-cli-zuglet/firebase-firestore.js');
-    app.import('vendor/ember-cli-zuglet/firebase-firestore.js.map');
-    app.import('vendor/ember-cli-zuglet/firebase-shim.js');
+
+    if(isFirebaseEnabled) {
+      app.import('vendor/ember-cli-zuglet/firebase.js');
+      app.import('vendor/ember-cli-zuglet/firebase.js.map');
+      app.import('vendor/ember-cli-zuglet/firebase-firestore.js');
+      app.import('vendor/ember-cli-zuglet/firebase-firestore.js.map');
+      app.import('vendor/ember-cli-zuglet/firebase-shim.js');
+    }
+
+    if(isLuxonEnabled) {
+      app.import('vendor/ember-cli-zuglet/luxon.js', {
+        using: [
+          { transformation: 'amd', as: 'luxon' }
+        ]
+      });
+    }
+
     app.import('vendor/ember-cli-zuglet/versions.js');
   },
   treeForVendor(vendorTree) {
@@ -28,20 +42,40 @@ module.exports = {
       trees.push(vendorTree);
     }
 
-    trees.push(create('ember-cli-zuglet/versions.js', [
+    let versions = [
       `Ember.libraries.register('${pkg.name}', '${pkg.version}');`,
-      `Ember.libraries.register('Firebase SDK', '${firebase.SDK_VERSION}');`
-    ].join('\n')));
+    ];
 
-    trees.push(new Funnel(path.dirname(require.resolve('firebase/firebase')), {
-      files: [
-        'firebase.js',
-        'firebase.js.map',
-        'firebase-firestore.js',
-        'firebase-firestore.js.map'
-      ],
-      destDir: '/ember-cli-zuglet'
-    }));
+    if(isFirebaseEnabled) {
+      trees.push(new Funnel(path.dirname(require.resolve('firebase/firebase')), {
+        files: [
+          'firebase.js',
+          'firebase.js.map',
+          'firebase-firestore.js',
+          'firebase-firestore.js.map'
+        ],
+        destDir: '/ember-cli-zuglet'
+      }));
+
+      let firebase = require('firebase');
+      versions.push(`Ember.libraries.register('Firebase SDK', '${firebase.SDK_VERSION}');`)
+    }
+
+    if(isLuxonEnabled) {
+      let luxon = path.resolve(path.dirname(require.resolve('luxon')), '..', '..');
+      trees.push(new Funnel(path.join(luxon, 'build', 'amd'), {
+        files: [
+          'luxon.js',
+          'luxon.js.map'
+        ],
+        destDir: '/ember-cli-zuglet'
+      }));
+
+      let pkg = require(path.join(luxon, 'package.json'));
+      versions.push(`Ember.libraries.register('Luxon', '${pkg.version}');`)
+    }
+
+    trees.push(create('ember-cli-zuglet/versions.js', versions.join('\n')));
 
     return mergeTrees(trees);
   }
