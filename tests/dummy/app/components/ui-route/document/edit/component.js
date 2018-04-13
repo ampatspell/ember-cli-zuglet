7e104d502a51a998c0c09907f967a4fe90e3b9fd
ampatspell <ampatspell@gmail.com>
2018-04-14 01:09:49 +0300

    dummy

---
+++ tests/dummy/app/components/ui-route/document/edit/component.js
@@ -0,0 +1,39 @@
+import Component from '@ember/component';
+import layout from './template';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  layout,
+
+  doc: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.get('store').doc('messages/first').load({ optional: true }).then(doc => {
+      if(this.isDestroying) {
+        return;
+      }
+      let cancel = doc.observe();
+      this.setProperties({
+        doc,
+        cancel
+      });
+    });
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    let cancel = this.get('cancel');
+    cancel && cancel();
+  },
+
+  actions: {
+    save(doc) {
+      doc.save();
+    },
+    reset(doc) {
+      doc.reset();
+    }
+  }
+
+});
