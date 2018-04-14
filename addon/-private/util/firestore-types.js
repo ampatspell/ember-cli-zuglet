616295502bed2b4a4badc85dca158e32e005acb4
ampatspell <ampatspell@gmail.com>
2018-04-14 20:13:19 +0300

    improve serverTimestamp compare

--- addon/-private/util/firestore-types.js
+++ addon/-private/util/firestore-types.js
@@ -2,14 +2,16 @@ import firebase from 'firebase';
 
 const {
   DocumentReference,
-  CollectionReference
+  CollectionReference,
+  FieldValue
 } = firebase.firestore;
 
 export const isFirestoreDocumentReference = value => value instanceof DocumentReference;
 export const isFirestoreCollectionReference = value => value instanceof CollectionReference;
 
-export const isFirestoreDocumentOrCollectionReference = value => isFirestoreDocumentReference(value) || isFirestoreCollectionReference(value);
-
-export const isFirestoreServerTimestamp = value => {
-  return value && value.methodName === 'FieldValue.serverTimestamp()';
+export const isFirestoreDocumentOrCollectionReference = value => {
+  return isFirestoreDocumentReference(value) || isFirestoreCollectionReference(value);
 };
+
+const serverTimestamp = FieldValue.serverTimestamp();
+export const isFirestoreServerTimestamp = value => serverTimestamp.isEqual(value);
