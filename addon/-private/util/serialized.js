c977b496caf9facf059f390fdf20589b2e4ba450
ampatspell <ampatspell@gmail.com>
2018-04-14 16:12:48 +0300

    fix serialized keep

--- addon/-private/util/serialized.js
+++ addon/-private/util/serialized.js
@@ -1,7 +1,12 @@
 import { computed } from '@ember/object';
 
-export default (keys, keep=[]) => computed(...keys, function() {
+export default (keys, keep=true) => computed(...keys, function() {
   let props = this.getProperties(keys);
+
+  if(keep === true) {
+    return props;
+  }
+
   return keys.reduce((hash, key) => {
     let value = props[key];
     if(value !== undefined || keep.includes(key)) {
