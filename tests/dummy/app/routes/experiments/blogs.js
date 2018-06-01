8de54f41b259f77cad885382c538b8461af346b1
ampatspell <ampatspell@gmail.com>
2018-06-01 05:16:10 +0300

    move models inline

--- tests/dummy/app/routes/experiments/blogs.js
+++ tests/dummy/app/routes/experiments/blogs.js
@@ -1,8 +1,53 @@
 import Route from '@ember/routing/route';
 import { model } from 'ember-cli-zuglet/experimental/route';
+import { observed, observerPromiseFor } from 'ember-cli-zuglet/experimental/computed';
+import { all } from 'rsvp';
 
 export default Route.extend({
 
-  model: model()
+  model: model({
+
+    blogs: observed(),
+
+    init() {
+      this._super(...arguments);
+      console.log('init', this+'');
+    },
+
+    insert() {
+      let store = this.get('store');
+      let doc = (path, data) => store.doc(path).new(data).save();
+      let post = (blog, name) => doc(`blogs/${blog}/posts/${name}`, { blog, name });
+      let blog = name => all([
+        doc(`blogs/${name}`, { name }),
+        post(name, 'one'),
+        post(name, 'two'),
+      ]);
+      return all([
+        blog('amateurinmotion'),
+        blog('zeeba')
+      ]);
+    },
+
+    prepare() {
+      console.log('prepare', this+'');
+      // return this.insert();
+
+      let blogs = this.get('store').collection('blogs').query({ type: 'array' });
+
+      this.setProperties({
+        blogs
+      });
+
+      // return observerPromiseFor(this, 'blogs');
+      return this.get('blogs').load();
+    },
+
+    willDestroy() {
+      this._super(...arguments);
+      console.log('willDestroy', this+'');
+    }
+
+  })
 
 });
--- tests/dummy/app/routes/experiments/blogs/blog.js
+++ tests/dummy/app/routes/experiments/blogs/blog.js
@@ -1,15 +1,48 @@
 import Route from '@ember/routing/route';
 import { model } from 'ember-cli-zuglet/experimental/route';
+import { observed, observerPromiseFor } from 'ember-cli-zuglet/experimental/computed';
+import { reject } from 'rsvp';
 
 export default Route.extend({
 
-  model: model(null, (route, params) => {
-    let blogs = route.modelFor('experiments.blogs');
-    let id = params.blog_id;
-    return {
-      blogs,
-      id
+  model: model({
+
+    blog: null,
+    posts: observed(),
+
+    init() {
+      this._super(...arguments);
+      console.log('init', this+'');
+    },
+
+    prepare({ route, params }) {
+      console.log('prepare', this+'');
+
+      let blogs = route.modelFor('experiments.blogs');
+      let id = params.blog_id;
+
+      let blog = blogs.get('blogs.content').findBy('id', id);
+      if(!blog) {
+        return reject(new Error(`Blog '${id}' was not found`));
+      }
+
+      let posts = blog.get('ref').collection('posts').query({ type: 'array' });
+
+      this.setProperties({
+        blog,
+        posts
+      });
+
+      return posts.load();
+      // return oserverPromiseFor(this, 'posts');
+
+    },
+
+    willDestroy() {
+      this._super(...arguments);
+      console.log('willDestroy', this+'');
     }
+
   })
 
 });
