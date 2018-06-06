3c7fc623372fe49c2450e53167671f62209da231
ampatspell <ampatspell@gmail.com>
2018-06-06 05:24:34 +0300

    remove tx public api

--- addon/-private/transaction/transaction.js
+++ addon/-private/transaction/transaction.js
@@ -1,21 +1,5 @@
 import EmberObject from '@ember/object';
 import ModelMixin from '../internal/model-mixin';
 
-const forwardPromise = name => function(model, ...rest) {
-  let internal = this._internal;
-  return internal[name].call(internal, model && model._internal).then(internal => {
-    return internal && internal.model(true);
-  });
-}
-
-const forward = name => function(model, ...rest) {
-  let internal = this._internal;
-  return internal[name].call(internal, model && model._internal);
-}
-
 export default EmberObject.extend(ModelMixin, {
-
-  load: forwardPromise('load'),
-  save: forward('save'),
-
 });
