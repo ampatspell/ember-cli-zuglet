52dd61c9e2c8ea179f8d42f148eebd6785eb59e6
ampatspell <ampatspell@gmail.com>
2018-06-15 15:41:14 +0300

    remove last / from page id

--- tests/dummy/app/routes/docs/page.js
+++ tests/dummy/app/routes/docs/page.js
@@ -1,9 +1,16 @@
 import Route from '@ember/routing/route';
 
+const normalize = id => {
+  if(id.endsWith('/')) {
+    return id.slice(0, -1);
+  }
+  return id;
+}
+
 export default Route.extend({
 
   model(params) {
-    let id = params.page_id;
+    let id = normalize(params.page_id);
     if(id === 'index') {
       return this.transitionTo('index');
     }
