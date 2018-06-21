9f3e8176d290bbccbee022160c9ba4cd5d97d686
ampatspell <ampatspell@gmail.com>
2018-06-22 02:00:21 +0300

    update ui

--- tests/dummy/app/components/ui-block/pages/toc/node/component.js
+++ tests/dummy/app/components/ui-block/pages/toc/node/component.js
@@ -1,7 +1,14 @@
 import Component from '@ember/component';
 import layout from './template';
+import { sort } from '@ember/object/computed';
+
+const sortedDesc = [ 'pos:asc' ];
 
 export default Component.extend({
   tagName: '',
-  layout
+  layout,
+
+  sortedDesc,
+  sorted: sort('page.pages', 'sortedDesc')
+
 });
