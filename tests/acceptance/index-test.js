be89a9ed9fa3077bbf0ec2593aaf95e8ba9610d7
ampatspell <ampatspell@gmail.com>
2018-07-05 05:17:31 +0300

    fix index app test

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -5,27 +5,19 @@ import { setupApplicationTest } from 'ember-qunit';
 module('Acceptance | index', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('has route component', async function(assert) {
-    await visit('/');
-    assert.equal(currentURL(), '/');
-    assert.ok(find('.ui-route-index'));
-    assert.ok(find('.ui-route-index-header'));
-    assert.ok(find('.ui-route-index-navigation'));
-    assert.ok(find('.ui-route-index-content'));
-  });
-
-  test('has header', async function(assert) {
-    await visit('/');
-    assert.equal(find('.ui-route-index-header .title').textContent, 'ember-cli-zuglet');
-  });
-
   test('has links', async function(assert) {
     await visit('/');
-    let links = findAll('.ui-route-index-navigation a');
+    assert.equal(currentURL(), '/');
+    let links = findAll('.ui-route-index-section.links .content a');
     assert.equal(links.length, 3);
     assert.equal(links[0].pathname, '/docs');
     assert.equal(links[1].pathname, '/experiments');
     assert.equal(links[2].href, 'https://github.com/ampatspell/ember-cli-zuglet');
+  });
+
+  test('has header', async function(assert) {
+    await visit('/');
+    assert.equal(find('.ui-route-index-section.header .content .header .value').textContent, 'ember-cli-zuglet');
   });
 
 });
