16f4382cb7969793778421d92f92a94906784d6f
ampatspell <ampatspell@gmail.com>
2018-08-05 15:02:43 +0300

    update blueprint instance-initializer to use initialize instead of register

--- blueprints/ember-cli-zuglet/files/__root__/instance-initializers/__name__-store.js
+++ blueprints/ember-cli-zuglet/files/__root__/instance-initializers/__name__-store.js
@@ -1,24 +1,22 @@
-import register from 'ember-cli-zuglet/register';
+import { initialize } from 'ember-cli-zuglet/initialize';
 import Store from '../store';
 
 export default {
   name: '<%= dasherizedPackageName %>:store',
-  initialize(app) {
-    register({
-      app,
-      store: {
-        identifier: 'store',
-        factory: Store
-      },
-      // service: {
-      //   enabled: true,
-      //   name: 'store',
-      //   inject: [ 'route', 'controller', 'component' ],
-      // },
-      // development: {
-      //   enabled: true,
-      //   export: 'store'
-      // }
-    });
-  }
+  initialize: initialize({
+    store: {
+      identifier: 'store',
+      factory: Store
+    },
+    // service: {
+    //   enabled: true,
+    //   name: 'store',
+    //   inject: [ 'route', 'controller', 'component', 'model' ],
+    // },
+    // development: {
+    //   enabled: true,
+    //   export: 'store',
+    //   logging: true
+    // }
+  })
 };
