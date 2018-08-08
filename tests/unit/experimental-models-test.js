3073ac2f2d7d05ac62916f5ea836a82a299db9a3
ampatspell <ampatspell@gmail.com>
2018-08-08 20:08:29 +0300

    tests

--- tests/unit/experimental-models-test.js
+++ tests/unit/experimental-models-test.js
@@ -3,6 +3,7 @@ import { module, test, setupStoreTest } from '../helpers/setup';
 import { getOwner } from '@ember/application';
 import models from 'ember-cli-zuglet/experimental/models';
 import { run } from '@ember/runloop';
+import { A } from '@ember/array';
 
 const Owner = EmberObject.extend({
 });
@@ -34,7 +35,7 @@ module('experimental-models', function(hooks) {
 
   test('models content is empty if there is no source objects', async function(assert) {
     let subject = this.subject({
-      source: [],
+      source: A(),
       models: models('source', {})
     });
     let prop = subject.get('models');
@@ -42,6 +43,76 @@ module('experimental-models', function(hooks) {
     assert.deepEqual(prop.get('content'), []);
   });
 
+  test('models content stays the same array', async function(assert) {
+    let subject = this.subject({
+      source: A(),
+      models: models('source', {
+        prepare() {}
+      })
+    });
+
+    let prop = subject.get('models');
+    let content = prop.get('content');
+
+    assert.deepEqual(content, []);
+
+    subject.get('source').pushObject({ ok: true });
+
+    assert.ok(content === prop.get('content'));
+    assert.equal(content.get('length'), 1);
+  });
+
+  test('removed models are destroyed', async function(assert) {
+    let subject = this.subject({
+      source: A(),
+      models: models('source', {
+        prepare(raw) {
+          this.setProperties({ raw });
+        }
+      })
+    });
+
+    let source = subject.get('source');
+    let prop = subject.get('models');
+
+    source.pushObject({ id: 'first' });
+
+    assert.equal(prop.get('content.length'), 1);
+
+    let first = prop.get('content.firstObject');
+
+    assert.equal(first.get('raw.id'), 'first');
+    assert.ok(!first.isDestroying);
+
+    source.removeAt(0);
+
+    assert.equal(run(() => prop.get('content.length')), 0);
+    assert.ok(first.isDestroying);
+  });
+
+  test('models are destroyed on prop destroy', async function(assert) {
+    let subject = this.subject({
+      source: A([ { id: 'first' } ]),
+      models: models('source', {
+        prepare(raw) {
+          this.setProperties({ raw });
+        }
+      })
+    });
+
+    let prop = subject.get('models');
+
+    assert.equal(prop.get('content.length'), 1);
+    let first = prop.get('content.firstObject');
+    assert.equal(first.get('raw.id'), 'first');
+    assert.ok(!first.isDestroying);
+
+    run(() => subject.destroy());
+
+    assert.ok(first.isDestroying);
+  });
+
+  // inline without mapping
   // inline with mapping
   // resolved without mapping
   // resolved with mapping
