ce06a7577a1645bad22510bb50bd827a7471939d
ampatspell <ampatspell@gmail.com>
2018-08-08 21:39:35 +0300

    dummy

--- tests/dummy/app/components/ui-route/experiments/models/component.js
+++ tests/dummy/app/components/ui-route/experiments/models/component.js
@@ -1,49 +1,7 @@
 import Component from '@ember/component';
 import layout from './template';
-import model from 'ember-cli-zuglet/experimental/model';
-import models from 'ember-cli-zuglet/experimental/models';
-import observed from 'ember-cli-zuglet/experimental/observed';
-import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
   classNameBindings: [ ':ui-route-experiments-models' ],
-  layout,
-
-  model: model({
-
-    query: observed(),
-
-    models: models('query.content', 'data.type', {
-
-      doc: null,
-
-      id: readOnly('doc.id'),
-      type: readOnly('doc.data.type'),
-      name: readOnly('doc.data.name'),
-
-      prepare(doc) {
-        this.setProperties({ doc });
-      },
-
-      willDestroy() {
-        this._super(...arguments);
-        console.log(this+'', 'willDestroy');
-      }
-
-    }),
-
-    prepare() {
-      let query = this.store.collection('ducks').query();
-      this.setProperties({ query });
-      window.model = this;
-      window.models = this.models;
-    },
-
-    willDestroy() {
-      this._super(...arguments);
-      console.log(this+'', 'willDestroy');
-    }
-
-  }),
-
+  layout
 });
---
+++ tests/dummy/app/routes/experiments/models.js
@@ -0,0 +1,61 @@
+import Route from '@ember/routing/route';
+import model from 'ember-cli-zuglet/experimental/model/route';
+import models from 'ember-cli-zuglet/experimental/models';
+import observed from 'ember-cli-zuglet/experimental/observed';
+import { readOnly } from '@ember/object/computed';
+
+export default Route.extend({
+
+  model: model({
+
+    query: observed(),
+
+    hamsters: models('query.content', {
+
+      doc: null,
+
+      id: readOnly('doc.id'),
+      type: readOnly('doc.data.type'),
+      name: readOnly('doc.data.name'),
+
+      prepare(doc) {
+        this.setProperties({ doc });
+      }
+
+    }),
+
+    insert() {
+      let { isLoaded, size } = this.get('query').getProperties('isLoaded', 'size');
+
+      if(!isLoaded) {
+        return;
+      }
+
+      if(size > 0) {
+        return;
+      }
+
+      let coll = this.query.ref;
+
+      return this.store.batch(batch => {
+        const save = hash => batch.save(coll.doc().new(hash));
+        save({ type: 'regular', name: 'Green' });
+        save({ type: 'regular', name: 'Yellow' });
+        save({ type: 'regular', name: 'White' });
+        save({ type: 'special', name: 'Orange' });
+        save({ type: 'regular', name: 'Brown' });
+        save({ type: 'regular', name: 'Pink' });
+        save({ type: 'regular', name: 'Red' });
+      });
+    },
+
+    prepare() {
+      let query = this.store.collection('hamsters').query();
+      this.setProperties({ query });
+
+      return query.observers.promise.then(() => this.insert());
+    }
+
+  })
+
+});
