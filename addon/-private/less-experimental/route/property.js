b3d69db4353119022c202382233b150f879ee45b
ampatspell <ampatspell@gmail.com>
2018-08-19 22:49:37 +0300

    explicitly destroy model on aborted transitions

--- addon/-private/less-experimental/route/property.js
+++ addon/-private/less-experimental/route/property.js
@@ -1,11 +1,16 @@
 import { getOwner } from '@ember/application';
 import { onResetController } from './hooks';
 import { getInternal } from './internal';
+import { resolve } from 'rsvp';
+
+const destroyInternal = internal => {
+  internal && internal.destroy();
+}
 
 export const resetController = function() {
   let model = this.currentModel;
   let internal = getInternal(model, this);
-  internal && internal.destroy();
+  destroyInternal(internal);
 }
 
 const create = (route, params, opts) => {
@@ -14,9 +19,16 @@ const create = (route, params, opts) => {
 }
 
 export default opts => {
-  return function(params) {
+  return function(params, transition) {
     onResetController(this, resetController);
     let internal = create(this, params, opts);
-    return internal.load();
+    let promise = resolve(internal.load());
+    promise.catch(() => {}).finally(() => {
+      if(!transition.isAborted) {
+        return;
+      }
+      destroyInternal(internal);
+    });
+    return promise;
   }
 }
