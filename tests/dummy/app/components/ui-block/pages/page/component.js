cc481db4c0bc20c0765b36a1ba88ff14d58c48c3
ampatspell <ampatspell@gmail.com>
2018-08-23 03:34:18 +0300

    scroll to content on click

--- tests/dummy/app/components/ui-block/pages/page/component.js
+++ tests/dummy/app/components/ui-block/pages/page/component.js
@@ -1,7 +1,20 @@
 import Component from '@ember/component';
 import layout from './template';
+import { observer } from '@ember/object';
+import $ from 'jquery';
 
 export default Component.extend({
   classNameBindings: [ ':ui-block-pages-page' ],
-  layout
+  layout,
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.scrollToContent();
+  },
+
+  scrollToContent: observer('page', function() {
+    let { top } = $(this.element).offset();
+    window.scrollTo(0, top - 30);
+  })
+
 });
