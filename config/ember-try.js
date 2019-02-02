ff8ae88870b3c2e4119d9af213639c36f2955f88
Arnis Vuskans <ampatspell@gmail.com>
2019-02-02 15:23:50 +0200

    travis setup

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,17 +13,21 @@ module.exports = function() {
       scenarios: [
         {
           name: 'ember-lts-2.18',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+          },
           npm: {
             devDependencies: {
+              '@ember/jquery': '^0.5.1',
               'ember-source': '~2.18.0'
             }
           }
         },
         {
-          name: 'ember-3.3.2',
+          name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
-              'ember-source': '3.3.2'
+              'ember-source': '~3.4.0'
             }
           }
         },
