ec0dcc10807e98060485b1f983aa86d44197f88f
Arnis Vuskans <ampatspell@gmail.com>
2019-10-28 21:35:28 +0200

    untested multiple baches

--- addon/-private/batch/internal.js
+++ addon/-private/batch/internal.js
@@ -1,15 +1,77 @@
 import Internal from '../internal/internal';
-import { resolve } from 'rsvp';
+import { resolve, all } from 'rsvp';
+import { assign } from '@ember/polyfills';
+
+const normalize = args => {
+  let opts;
+  let fn = null;
+  if(args.length === 2) {
+    fn = args[0];
+    opts = args[1];
+  } else if(args.length === 1) {
+    if(typeof args[0] === 'function') {
+      fn = args[0];
+    } else {
+      opts = args[0];
+    }
+  }
+  opts = assign({ multiple: false }, opts);
+  return {
+    fn,
+    opts
+  };
+}
+
+class Batch {
+
+  constructor(firestore, limit) {
+    this._firestore = firestore;
+    this._limit = limit;
+    this._count = 0;
+    this._instances = [];
+  }
+
+  _batch() {
+    return this._firestore.batch();
+  }
+
+  _increment() {
+    this.count++;
+    let { _count, _limit, _instances } = this;
+    if(_count > _limit || !_instances.length) {
+      this._count = 1;
+      _instances.push(this._batch());
+    }
+    return _instances[_instances.length - 1];
+  }
+
+  set(...args) {
+    return this._increment().set(...args);
+  }
+
+  delete(...args) {
+    return this._increment().delete(...args);
+  }
+
+  commit() {
+    return all(this._instances.map(instance => instance.commit())).then(() => undefined);
+  }
+
+}
 
 export default Internal.extend({
 
   store: null,
   instance: null,
+  args: null,
+  opts: null,
   fn: null,
 
   init() {
     this._super(...arguments);
-    this.instance = this.store.app.firestore().batch();
+    let { opts, fn } = normalize(this.args);
+    this.setProperties({ opts, fn });
+    this.instance = new Batch(this.store.app.firestore(), opts.multiple ? 500 : Infinity);
   },
 
   createModel() {
