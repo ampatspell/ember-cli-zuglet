78ebe0ef3e6bc67b76be37b4d30caf76d755a02a
Arnis Vuskans <ampatspell@gmail.com>
2020-10-25 02:14:22 +0300

    associate state and properties to be destroyed with model

--- addon/-private/state/state.js
+++ addon/-private/state/state.js
@@ -1,14 +1,21 @@
 import EmberObject from '@ember/object';
+import { associateDestroyableChild } from '@ember/destroyable';
 
 export default class State extends EmberObject {
 
   owner = null
   properties = Object.create(null);
 
+  init() {
+    super.init(...arguments);
+    associateDestroyableChild(this.owner, this);
+  }
+
   getProperty(key, create) {
     let property = this.properties[key];
     if(!property && create) {
       property = create();
+      associateDestroyableChild(this, property);
       this.properties[key] = property;
     }
     return property;
