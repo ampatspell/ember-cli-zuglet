4139ab940a2a5a047e69c5f04a00d0fce3e4dbdb
Arnis Vuskans <ampatspell@gmail.com>
2020-10-25 02:17:25 +0300

    Update get-state.js

--- addon/-private/state/get-state.js
+++ addon/-private/state/get-state.js
@@ -6,13 +6,13 @@ const createState = owner => {
   return getOwner(owner).factoryFor('zuglet:state').create({ owner });
 }
 
-export const getState = owner => {
+export const getState = (owner, create=true) => {
   if(!owner) {
     return;
   }
 
   let state = owner[_state];
-  if(!state) {
+  if(!state && create) {
     state = createState(owner);
     owner[_state] = state;
   }
