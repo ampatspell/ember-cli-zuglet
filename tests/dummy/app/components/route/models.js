589817a8833fcc4257e0b9577cd782452effb6ee
Arnis Vuskans <ampatspell@gmail.com>
2020-10-26 03:10:23 +0200

    dummy

--- tests/dummy/app/components/route/models.js
+++ tests/dummy/app/components/route/models.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { setGlobal, toString } from 'zuglet/utils';
 import { root, activate, models } from 'zuglet/decorators';
+import { action } from '@ember/object';
 
 @root()
 export default class RouteModelsComponent extends Component {
@@ -19,6 +20,13 @@ export default class RouteModelsComponent extends Component {
     super(...arguments);
     this.query = this.store.collection('messages').query();
     setGlobal({ component: this });
+  }
+
+  @action
+  async add() {
+    await this.store.collection('messages').doc().new({
+      name: 'new message'
+    }).save();
   }
 
   toString() {
