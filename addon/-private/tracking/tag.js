626ea1808fbda2fe5ae8fac975d996d9cfdd383c
Arnis Vuskans <ampatspell@gmail.com>
2020-10-26 22:47:08 +0200

    tag-based consume & dirty

---
+++ addon/-private/tracking/tag.js
@@ -0,0 +1,42 @@
+import { tracked } from '@glimmer/tracking';
+
+class Tag {
+
+  @tracked
+  __tag__
+
+  consume() {
+    this.__tag__
+  }
+
+  dirty() {
+    this.__tag__ = undefined;
+  }
+
+}
+
+export const TAGS = new WeakMap();
+
+const getTag = (object, key) => {
+  let tags = TAGS.get(object);
+  if(!tags) {
+    tags = new Map();
+    TAGS.set(object, tags);
+  }
+
+  let tag = tags.get(key);
+  if(!tag) {
+    tag = new Tag();
+    tags.set(key, tag);
+  }
+
+  return tag;
+}
+
+export const consumeKey = (object, key) => {
+  getTag(object, key).consume();
+}
+
+export const dirtyKey = (object, key) => {
+  getTag(object, key).dirty();
+}
--- tests/dummy/app/components/route/dev.js
+++ tests/dummy/app/components/route/dev.js
@@ -1,13 +1,42 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { setGlobal, toString } from 'zuglet/utils';
 import { tracked } from '@glimmer/tracking';
+import { consumeKey, dirtyKey } from 'zuglet/-private/tracking/tag';
+import { alias } from 'macro-decorators';
 
 export default class RouteDevComponent extends Component {
 
   @service
   store
+
+  @tracked
+  isDirty = false
+
+  _data = { name: 'hey there' }
+
+  get data() {
+    let { _proxy } = this;
+    if(!_proxy) {
+      _proxy = new Proxy(this._data, {
+        get: (target, prop) => {
+          consumeKey(target, prop);
+          return target[prop];
+        },
+        set: (target, prop, value) => {
+          target[prop] = value;
+          dirtyKey(target, prop);
+          this.isDirty = true;
+          return true;
+        }
+      })
+      this._proxy = _proxy;
+    }
+    return _proxy;
+  }
+
+  @alias('data.name')
+  name
 
   constructor() {
     super(...arguments);
