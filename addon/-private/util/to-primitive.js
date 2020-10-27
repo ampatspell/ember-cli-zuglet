d0678b4c56171af80b778dee7225fb3b885a97da
Arnis Vuskans <ampatspell@gmail.com>
2020-10-28 01:22:18 +0200

    fix toPrimitive which is used in asserts and may not have arg

--- addon/-private/util/to-primitive.js
+++ addon/-private/util/to-primitive.js
@@ -1,5 +1,8 @@
 import { guidFor } from '@ember/object/internals';
 
 export const toPrimitive = model => {
+  if(!model) {
+    return;
+  }
   return `${model.constructor.name}::${guidFor(model)}`;
 }
