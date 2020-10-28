d72cb58c4d9d03b373592bb6b2140de7a07dbc28
Arnis Vuskans <ampatspell@gmail.com>
2020-10-28 05:43:25 +0200

    dummy

--- tests/dummy/app/components/route/route.js
+++ tests/dummy/app/components/route/route.js
@@ -4,11 +4,6 @@ import { action } from '@ember/object';
 export default class RouteRouteComponent extends Component {
 
   @action
-  async save(message) {
-    await message.save();
-  }
-
-  @action
   async add() {
     await this.args.model.add('Untitled');
   }
