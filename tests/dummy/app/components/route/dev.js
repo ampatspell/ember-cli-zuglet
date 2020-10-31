d9aed0ffee5191874922970d2014e8a15c88b9a9
ampatspell <ampatspell@gmail.com>
2020-10-31 14:29:01 +0200

    dummy createCache experiment

--- tests/dummy/app/components/route/dev.js
+++ tests/dummy/app/components/route/dev.js
@@ -2,12 +2,40 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { setGlobal, toString } from 'zuglet/utils';
 import { root } from 'zuglet/decorators';
+import { tracked } from '@glimmer/tracking';
+import { createCache, getValue } from '@glimmer/tracking/primitives/cache';
+
+const cached = () => (target, _key, descriptor) => {
+  let key = `_${_key}`;
+  return {
+    get() {
+      let cache = this[key];
+      if(!cache) {
+        cache = createCache(descriptor.get.bind(this));
+        this[key] = cache;
+      }
+      return getValue(cache);
+    }
+  };
+}
 
 @root()
 export default class RouteDevComponent extends Component {
 
   @service
   store
+
+  @tracked
+  greeting = 'Hello'
+
+  @tracked
+  name = 'zeeba'
+
+  @cached()
+  get thing() {
+    console.log('recompute');
+    return `${this.greeting} ${this.name}`;
+  }
 
   constructor() {
     super(...arguments);
