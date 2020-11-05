a1b811510977a0a2ddeba288eab18b3691c130bf
Arnis Vuskans <ampatspell@gmail.com>
2020-11-05 22:13:40 +0200

    cleanup

--- tests/dummy/app/components/route/dev.js
+++ tests/dummy/app/components/route/dev.js
@@ -1,8 +1,7 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { setGlobal, toString } from 'zuglet/utils';
-import { root, activate } from 'zuglet/decorators';
-import { action } from "@ember/object";
+import { root } from 'zuglet/decorators';
 
 @root()
 export default class RouteDevComponent extends Component {
@@ -10,24 +9,9 @@ export default class RouteDevComponent extends Component {
   @service
   store
 
-  @activate().content(({ store }) => store.doc('messages/thingie').existing())
-  doc
-
   constructor() {
     super(...arguments);
-    this.doc.onData(doc => console.log('onData'));
-    this.doc.onDeleted(doc => console.log('onDeleted'));
     setGlobal({ component: this });
-  }
-
-  @action
-  async save() {
-    await this.doc.save({ force: true });
-  }
-
-  @action
-  async delete() {
-    await this.doc.delete();
   }
 
   toString() {
