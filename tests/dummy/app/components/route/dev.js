ecfbe9ebccf64a861e6d490837958e99d80e9993
ampatspell <ampatspell@gmail.com>
2020-11-06 03:37:42 +0200

    dummy

--- tests/dummy/app/components/route/dev.js
+++ tests/dummy/app/components/route/dev.js
@@ -1,16 +1,13 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { setGlobal, toString } from 'zuglet/utils';
-import { root, activate } from 'zuglet/decorators';
+import { root } from 'zuglet/decorators';
 
 @root()
 export default class RouteDevComponent extends Component {
 
   @service
   store
-
-  @activate().content(({ store }) => store.collection('messages').query().passive())
-  doc
 
   constructor() {
     super(...arguments);
