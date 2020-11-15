f8f0360f9445d8b85477f612d9b18478619d3bb9
ampatspell <ampatspell@gmail.com>
2020-11-15 02:54:00 +0000

    store test

---
+++ tests/unit/store-test.js
@@ -0,0 +1,34 @@
+import { module, test, setupStoreTest } from '../helpers/setup';
+
+module('store', function(hooks) {
+  setupStoreTest(hooks);
+
+  hooks.beforeEach(function(assert) {
+    this.projectId = this.store.options.firebase.projectId;
+    assert.ok(this.projectId);
+  });
+
+  test('serialized', function(assert) {
+    assert.deepEqual(this.store.serialized, {
+      identifier: 'test',
+      projectId: this.projectId
+    });
+  });
+
+  test('toJSON', function(assert) {
+    let json = this.store.toJSON();
+    assert.deepEqual(json, {
+      instance: json.instance,
+      serialized: {
+        identifier: 'test',
+        projectId: this.projectId
+      }
+    });
+    assert.ok(json.instance.startsWith('TestStore::ember'));
+  });
+
+  test('toStringExtension', function(assert) {
+    assert.strictEqual(this.store.toStringExtension(), this.projectId);
+  });
+
+});
