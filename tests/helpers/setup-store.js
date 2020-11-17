468b3ebe58e1dc737e8a03a03cd3083bac451d14
ampatspell <ampatspell@gmail.com>
2020-11-17 17:02:28 +0000

    test setup

--- tests/helpers/setup-store.js
+++ tests/helpers/setup-store.js
@@ -2,6 +2,7 @@ import Store from 'zuglet/store';
 import User from 'zuglet/user';
 import { activate, load } from 'zuglet/utils';
 import { getStats } from 'zuglet/-private/stores/stats';
+import { next } from 'zuglet/-private/util/runloop';
 import environment from '../../config/environment';
 
 let {
@@ -31,18 +32,18 @@ class TestStore extends Store {
 
 }
 
-let danglingPromisesMessage = len => {
+let pluralizeDangling = (len, singular, plural) => {
   if(len === 0) {
-    return 'no dangling promises';
+    return `no dangling ${plural}`;
   } else if(len === 1) {
-    return '1 dangling promise';
-  } else {
-    return `${len} dangling promises`;
+    return `1 dangling ${singular}`;
   }
+  return `${len} dangling ${plural}`;
 }
 
 export const setupStore = (hooks, identifier='test') => {
   hooks.beforeEach(async function() {
+    this.stores = this.owner.lookup('zuglet:stores');
     this.owner.register('model:test-user', TestUser);
     this.store = this.stores.createStore(identifier, TestStore);
     this.activations = [];
@@ -52,10 +53,13 @@ export const setupStore = (hooks, identifier='test') => {
     this.activate(this.store);
     await this.store.load();
   });
-  hooks.afterEach(function(assert) {
-    let stats = getStats(this.store);
-    assert.ok(!stats.hasPromises, danglingPromisesMessage(stats.promises.length));
+  hooks.afterEach(async function(assert) {
     this.activations.forEach(cancel => cancel());
-    // TODO: #31 @ampatspell assert there are no onSnapshot listeners after store is deactivated and this.activations are done
+    this.stores.destroy();
+    await next();
+
+    let stats = getStats(this.store);
+    assert.ok(!stats.hasPromises, pluralizeDangling(stats.promises.length, 'promise', 'promises'));
+    assert.ok(!stats.hasSnapshots, pluralizeDangling(stats.snapshots.length, 'snapshot', 'snapshots'));
   });
 }
--- tests/helpers/setup-stores.js
+++
@@ -1,8 +0,0 @@
-export const setupStores = hooks => {
-  hooks.beforeEach(function() {
-    this.stores = this.owner.lookup('zuglet:stores');
-  });
-  hooks.afterEach(function() {
-    this.stores.destroy();
-  });
-}
