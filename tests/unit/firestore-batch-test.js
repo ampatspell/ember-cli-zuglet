1a9884d3ab0a4e67c787111e2c84e2891e06510f
ampatspell <ampatspell@gmail.com>
2020-11-17 21:31:35 +0000

    more batch tests

--- tests/unit/firestore-batch-test.js
+++ tests/unit/firestore-batch-test.js
@@ -8,7 +8,7 @@ module('firestore / batch', function(hooks) {
     let coll = this.store.collection('ducks');
     await replaceCollection(coll, []);
 
-    let ret = await this.store.batch(batch => {
+    let ret = await this.store.batch(async batch => {
       batch.save(coll.doc('yellow').new({ name: 'yellow' }));
       return { ok: true };
     });
@@ -31,10 +31,61 @@ module('firestore / batch', function(hooks) {
     assert.deepEqual(data, { name: 'yellow' });
   });
 
-  // batch.save
-  // batch.delete
-  // batch updates doc state after commit resolves
-  // batch save takes doc
-  // batch delete takes doc or ref
+  test('save doc', async function(assert) {
+    let coll = this.store.collection('ducks');
+    await replaceCollection(coll, []);
+
+    let doc = coll.doc('yellow').new({ name: 'yellow' });
+
+    await this.store.batch(batch => {
+      let ret = batch.save(doc);
+      assert.ok(ret === doc);
+      assert.strictEqual(doc.isSaving, true);
+      assert.strictEqual(doc.exists, undefined);
+    });
+
+    assert.strictEqual(doc.isSaving, false);
+    assert.strictEqual(doc.exists, true);
+
+    let data = await coll.doc('yellow').data();
+    assert.deepEqual(data, { name: 'yellow' });
+  });
+
+  test('delete doc', async function(assert) {
+    let coll = this.store.collection('ducks');
+    await replaceCollection(coll, [
+      { _id: 'yellow', name: 'yellow' }
+    ]);
+
+    let doc = await coll.doc('yellow').load();
+
+    await this.store.batch(batch => {
+      batch.delete(doc);
+      assert.strictEqual(doc.isSaving, true);
+      assert.strictEqual(doc.exists, true);
+    });
+
+    assert.strictEqual(doc.isSaving, false);
+    assert.strictEqual(doc.exists, false);
+
+    let data = await doc.ref.data();
+    assert.ok(!data);
+  });
+
+  test('delete ref', async function(assert) {
+    let coll = this.store.collection('ducks');
+    await replaceCollection(coll, [
+      { _id: 'yellow', name: 'yellow' }
+    ]);
+
+    let ref = await coll.doc('yellow');
+
+    await this.store.batch(batch => {
+      batch.delete(ref);
+    });
+
+    let data = await ref.data();
+    assert.ok(!data);
+  });
 
 });
