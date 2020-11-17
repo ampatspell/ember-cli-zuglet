acaee14c524019e8da9ebb423a3072efbfdafc64
ampatspell <ampatspell@gmail.com>
2020-11-17 16:41:06 +0000

    move test this.activate to setup-store

--- tests/helpers/setup-helpers.js
+++ tests/helpers/setup-helpers.js
@@ -1,14 +1,5 @@
-import { activate } from 'zuglet/utils';
-
 export const setupHelpers = hooks => {
   hooks.beforeEach(function() {
     this.registerModel = (name, factory) => this.owner.register(`model:${name}`, factory);
-    this.activations = [];
-    this.activate = model => {
-      this.activations.push(activate(model));
-    }
-  });
-  hooks.afterEach(function() {
-    this.activations.forEach(cancel => cancel());
   });
 }
--- tests/helpers/setup-store.js
+++ tests/helpers/setup-store.js
@@ -31,27 +31,31 @@ class TestStore extends Store {
 
 }
 
+let danglingPromisesMessage = len => {
+  if(len === 0) {
+    return 'no dangling promises';
+  } else if(len === 1) {
+    return '1 dangling promise';
+  } else {
+    return `${len} dangling promises`;
+  }
+}
+
 export const setupStore = (hooks, identifier='test') => {
   hooks.beforeEach(async function() {
     this.owner.register('model:test-user', TestUser);
     this.store = this.stores.createStore(identifier, TestStore);
-    this.__cancelStoreActivation = activate(this.store);
+    this.activations = [];
+    this.activate = model => {
+      this.activations.push(activate(model));
+    }
+    this.activate(this.store);
     await this.store.load();
   });
   hooks.afterEach(function(assert) {
     let stats = getStats(this.store);
-    let message = () => {
-      let len = stats.promises.length;
-      if(len === 0) {
-        return 'no dangling promises';
-      } else if(len === 1) {
-        return '1 dangling promise';
-      } else {
-        return `${len} dangling promises`;
-      }
-    }
-    assert.ok(!stats.hasPromises, message());
-    this.__cancelStoreActivation();
+    assert.ok(!stats.hasPromises, danglingPromisesMessage(stats.promises.length));
+    this.activations.forEach(cancel => cancel());
     // TODO: #31 @ampatspell assert there are no onSnapshot listeners after store is deactivated and this.activations are done
   });
 }
