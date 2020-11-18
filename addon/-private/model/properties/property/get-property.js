2873d6456afc5306a6f8e58b83d15c6d9068ef25
ampatspell <ampatspell@gmail.com>
2020-11-18 18:20:16 +0000

    remove unused getProperty helper

--- addon/-private/model/properties/property/get-property.js
+++
@@ -1,6 +0,0 @@
-import { getState } from '../../state';
-
-export const getProperty = (owner, key) => {
-  let state = getState(owner);
-  return state && state.getProperty(key);
-}
--- addon/-private/model/properties/property/index.js
+++ addon/-private/model/properties/property/index.js
@@ -1,10 +1,8 @@
 import Property from './property';
 import { property, createProperty } from './decorator';
-import { getProperty } from './get-property';
 
 export {
   property,
-  getProperty,
   createProperty
 }
 
