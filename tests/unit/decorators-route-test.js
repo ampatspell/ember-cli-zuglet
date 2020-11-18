47a35e6a8a44e749a970786f07d27566977c94e8
ampatspell <ampatspell@gmail.com>
2020-11-18 19:15:39 +0000

    route decorator tests

---
+++ tests/unit/decorators-route-test.js
@@ -0,0 +1,158 @@
+import { module, test, setupStoreTest } from '../helpers/setup';
+import EmberObject from '@ember/object';
+import { route as routeDecorator } from 'zuglet/decorators';
+import { isActivated } from 'zuglet/utils';
+
+module('decorators / @route', function(hooks) {
+  setupStoreTest(hooks);
+
+  hooks.beforeEach(function(assert) {
+
+    class BaseRoute extends EmberObject {
+      resetController() {}
+    }
+
+    @routeDecorator()
+    class Route extends BaseRoute {
+
+      async model(arg) {
+        assert.ok(isActivated(this));
+        assert.ok(!isActivated(arg));
+        this.onModel && this.onModel();
+        return arg;
+      }
+
+      async load(model) {
+        assert.ok(isActivated(this));
+        assert.ok(isActivated(model));
+        await model.load();
+      }
+
+    }
+
+    class Model extends EmberObject {
+
+      async load() {
+        assert.ok(isActivated(this));
+        this.onLoad && this.onLoad();
+        this.loaded = true;
+      }
+
+    }
+
+    this.registerModel('route', Route);
+    this.registerModel('model', Model);
+  });
+
+  test('activate, load and deactivate', async function(assert) {
+    let route = this.store.models.create('route');
+    let model = this.store.models.create('model');
+
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+
+    route.transition = { isAborted: false };
+
+    await route.model(model, route.transition);
+
+    assert.ok(model.loaded);
+    assert.ok(isActivated(route));
+    assert.ok(isActivated(model));
+
+    route.resetController(null, true);
+
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+  });
+
+  test('aborted model deactivates', async function(assert) {
+    let route = this.store.models.create('route');
+    let model = this.store.models.create('model');
+
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+
+    let transition = { isAborted: false };
+
+    route.onModel = () => {
+      transition.isAborted = true;
+    }
+
+    await route.model(model, transition);
+
+    assert.ok(!model.loaded);
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+  });
+
+  test('thrown model deactivates', async function(assert) {
+    let route = this.store.models.create('route');
+    let model = this.store.models.create('model');
+
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+
+    let transition = { isAborted: false };
+
+    route.onModel = () => {
+      throw new Error('done');
+    }
+
+    try {
+      await route.model(model, transition);
+      assert.ok(false);
+    } catch(err) {
+      assert.strictEqual(err.message, 'done');
+    }
+
+    assert.ok(!model.loaded);
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+  });
+
+  test('aborted load deactivates', async function(assert) {
+    let route = this.store.models.create('route');
+    let model = this.store.models.create('model');
+
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+
+    let transition = { isAborted: false };
+
+    model.onLoad = () => {
+      transition.isAborted = true;
+    }
+
+    await route.model(model, transition);
+
+    assert.ok(model.loaded);
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+  });
+
+  test('thrown load deactivates', async function(assert) {
+    let route = this.store.models.create('route');
+    let model = this.store.models.create('model');
+
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+
+    let transition = { isAborted: false };
+
+    model.onLoad = () => {
+      throw new Error('done');
+    }
+
+    try {
+      await route.model(model, transition);
+      assert.ok(false);
+    } catch(err) {
+      assert.strictEqual(err.message, 'done');
+    }
+
+    assert.ok(!model.loaded);
+    assert.ok(!isActivated(route));
+    assert.ok(!isActivated(model));
+  });
+
+});
