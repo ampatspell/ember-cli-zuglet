b99448a9be635976102bee6f83fd3a9ee31010a3
ampatspell <ampatspell@gmail.com>
2020-11-18 20:28:40 +0000

    ignore coverage for setGlobal

--- addon/-private/util/set-global.js
+++ addon/-private/util/set-global.js
@@ -1,4 +1,8 @@
+/* istanbul ignore next */
 export const setGlobal = (hash, silent=false) => {
+  if(typeof window === 'undefined') {
+    return;
+  }
   for(let key in hash) {
     let value = hash[key];
     if(!silent) {
