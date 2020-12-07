96deeca7969e729cdef6b3aabacc9c59091cd6d0
ampatspell <ampatspell@gmail.com>
2020-12-07 14:59:42 +0000

    references as native

--- addon/-private/store/firestore/references/reference.js
+++ addon/-private/store/firestore/references/reference.js
@@ -1,10 +1,16 @@
-import EmberObject from '@ember/object';
+import ZugletObject from '../../../../object';
 import { toJSON } from '../../../util/to-json';
 
-export default class Reference extends EmberObject {
+export default class Reference extends ZugletObject {
 
   store = null
   _ref = null
+
+  constructor(owner, { store, _ref }) {
+    super(owner);
+    this.store = store;
+    this._ref = _ref;
+  }
 
   toJSON() {
     let { serialized } = this;
