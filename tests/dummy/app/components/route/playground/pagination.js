9ea43a084daba4da728f116f68835be31dca245c
ampatspell <ampatspell@gmail.com>
2020-12-27 17:22:13 +0000

    basic proof of concept for startAfter

--- tests/dummy/app/components/route/playground/pagination.js
+++ tests/dummy/app/components/route/playground/pagination.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { root } from 'zuglet/decorators';
+import { root, activate } from 'zuglet/decorators';
 import { setGlobal, toString, alive } from 'zuglet/utils';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
@@ -11,9 +11,27 @@ export default class RoutePlaygroundPaginationComponent extends Component {
   @service
   store
 
+  @activate()
+  queries
+
+  get ref() {
+    return this.store.collection('posts').orderBy('position', 'asc').limit(5);
+  }
+
   constructor() {
     super(...arguments);
     setGlobal({ component: this });
+    this.queries = [];
+    let query = this.ref.query();
+    this.queries.push(query);
+  }
+
+  @action
+  loadMore() {
+    let lastObject = arr => arr[arr.length - 1];
+    let last = lastObject(lastObject(this.queries).content);
+    let query = this.ref.startAfter(last).query();
+    this.queries.push(query);
   }
 
   toString() {
