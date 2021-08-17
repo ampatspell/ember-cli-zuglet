45dc0952e6853aa2281225ee2b3699bc69dc7db6
ampatspell <ampatspell@gmail.com>
2021-08-17 18:29:03 +0300

    experiments

--- tests/dummy/app/components/route/playground/data.js
+++ tests/dummy/app/components/route/playground/data.js
@@ -6,6 +6,32 @@ import { action } from '@ember/object';
 import { tracked } from "@glimmer/tracking";
 import { cached } from "tracked-toolbox";
 
+class RemoteDocumentUpdates {
+
+  constructor(owner, key) {
+    this.owner = owner;
+    this.key = key;
+    this.start();
+  }
+
+  onData(doc, { source, fromCache }) {
+    console.log({
+      data: doc.serialized.data,
+      source,
+      fromCache
+    });
+  }
+
+  start() {
+    this._cancel = this.owner[this.key].onData((doc, opts) => this.onData(doc, opts));
+  }
+
+  stop() {
+    this._cancel();
+  }
+
+}
+
 @root()
 export default class RoutePlaygroundDataComponent extends Component {
 
@@ -29,20 +55,25 @@ export default class RoutePlaygroundDataComponent extends Component {
   @tracked name = null;
 
   onActivated() {
-    setTimeout(() => {
-      this._onData = this.doc.onData((doc, opts) => console.log('onData', doc+'', opts));
-    }, 0);
+    this.onData = new RemoteDocumentUpdates(this, 'doc');
   }
 
   onDeactivated() {
-    this._onData();
+    this.onData.stop();
   }
 
   @action
-  async save() {
+  async saveLocal() {
+    let { doc } = this;
+    doc.data.name = this.name;
+    await doc.save({ token: true });
+  }
+
+  @action
+  async saveRemote() {
     let doc = this.ref.existing();
     doc.data.name = this.name;
-    await doc.save();
+    await doc.save({ token: true });
   }
 
   toString() {
