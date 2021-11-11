ce6844c19490a3c8e8ea6689c9eb78144b96ea93
ampatspell <ampatspell@gmail.com>
2021-11-11 04:22:59 +0200

    remove ember-classic and jquery from ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -46,37 +46,37 @@ module.exports = async function() {
           },
         },
       },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
+      // {
+      //   name: 'ember-default-with-jquery',
+      //   env: {
+      //     EMBER_OPTIONAL_FEATURES: JSON.stringify({
+      //       'jquery-integration': true,
+      //     }),
+      //   },
+      //   npm: {
+      //     devDependencies: {
+      //       '@ember/jquery': '^1.1.0',
+      //     },
+      //   },
+      // },
+      // {
+      //   name: 'ember-classic',
+      //   env: {
+      //     EMBER_OPTIONAL_FEATURES: JSON.stringify({
+      //       'application-template-wrapper': true,
+      //       'default-async-observers': false,
+      //       'template-only-glimmer-components': false,
+      //     }),
+      //   },
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '~3.28.0',
+      //     },
+      //     ember: {
+      //       edition: 'classic',
+      //     },
+      //   },
+      // },
       embroiderSafe(),
       // embroiderOptimized(),
     ],
