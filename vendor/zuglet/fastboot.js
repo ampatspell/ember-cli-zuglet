f9a7c8305ab4bb6f00a544e2df04a3ce90a070ce
ampatspell <ampatspell@gmail.com>
2021-11-11 17:46:36 +0200

    dummy test to check if ember-auto-import@1.x with fastboot works

--- vendor/zuglet/fastboot.js
+++ vendor/zuglet/fastboot.js
@@ -3,6 +3,7 @@
 if(typeof FastBoot !== 'undefined') {
   self.XMLHttpRequest = FastBoot.require('xmlhttprequest').XMLHttpRequest;
   self.fetch = FastBoot.require('node-fetch');
+  self.Headers = FastBoot.require('fetch-headers');
   if(typeof self.atob === 'undefined') {
     self.atob = string =>  FastBoot.require('buffer').Buffer.from(string, 'base64').toString('binary');
   }
