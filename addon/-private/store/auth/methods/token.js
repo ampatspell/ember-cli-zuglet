c4b18fd78adcd3812cc463698254c8f2ce491675
ampatspell <ampatspell@gmail.com>
2022-03-24 16:21:33 +0200

    sign in with custom token

---
+++ addon/-private/store/auth/methods/token.js
@@ -0,0 +1,13 @@
+import AuthMethod from './method';
+import { registerPromise } from '../../../stores/stats';
+
+export default class TokenAuthMethod extends AuthMethod {
+
+  signIn(token) {
+    return this.auth._withAuthReturningUser(async auth => {
+      let { user } = await registerPromise(this, 'sign-in', auth.signInWithCustomToken(token));
+      return { user };
+    });
+  }
+
+}
