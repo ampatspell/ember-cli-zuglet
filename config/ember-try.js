b0b8599136ee4ea51303330c1daf36b4b3c702b2
ampatspell <ampatspell@gmail.com>
2023-01-18 19:21:53 +0200

    skip release and beta for now

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
+// const getChannelURL = require('ember-source-channel-url');
 const { embroiderSafe, /* embroiderOptimized */ } = require('@embroider/test-setup');
 
 module.exports = async function() {
@@ -22,22 +22,22 @@ module.exports = async function() {
           },
         },
       },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
+      // {
+      //   name: 'ember-release',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('release')
+      //     }
+      //   }
+      // },
+      // {
+      //   name: 'ember-beta',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('beta'),
+      //     },
+      //   },
+      // },
       // {
       //   name: 'ember-canary',
       //   npm: {
